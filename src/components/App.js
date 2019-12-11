1296f7b876062549c981ae80a147575b017b51e5
jonas060831 <jonas_sulit2001@yahoo.com>
2019-12-10 16:44:29 -0800

    added redux devtools to react template

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//4. create the app component
 const App = () => {
     return <div className="ui container" >App</div>
 }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ import ReactDOM from 'react-dom';
 //1. Get the Provider and createStore functions
 //notice how we call it using named exports
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
+//components
 import App from './components/App';
 //2. create your reducers
 import reducers from './reducers';
 
+//3. redux dev tool
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-//3. Wrapped the App Component with the Provider Component from react-redux that we import then on the
-// Provider Component pass in the constant props named store
-//4. Pass the createStore functions inside the props named store and put reducers as first argument
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(thunk))
+)
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={ store }>
     <App />
     </Provider>,
     document.querySelector('#root')
