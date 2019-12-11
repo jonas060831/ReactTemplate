7fd9e13a75f94becac580203830304d7c843302a
jonas060831 <jonas_sulit2001@yahoo.com>
2019-12-10 19:19:12 -0800

    added react-router-dom for history

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 
-//4. create the app component
-const App = () => {
-    return <div className="ui container" >App</div>
+//history
+import history from '../history';
+
+//components
+import LandingPage from '../components/pages/main/LandingPage';
+
+class App extends Component {
+
+    render(){
+        return (
+            <div>
+                <Router history={ history } >
+                    <Switch>
+                        {/* UNPROTECTED ROUTES */}
+                            <Route path="/" exact component={LandingPage} />
+
+                        {/* AUTHENTICATED ROUTES */}
+
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
 
 export default App;
